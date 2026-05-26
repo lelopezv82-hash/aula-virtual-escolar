1941203ded6ff8860f59ce6df808694f70c1a3b1
lelopezv82-hash <lelopezv82@gmail.com>
2026-05-26 15:39:12 -0500

    deploy: upload app/docente/tareas/page.tsx

--- app/docente/tareas/page.tsx
+++ app/docente/tareas/page.tsx
@@ -1,10 +1,10 @@
-import { PrismaClient } from "@prisma/client";
+import prisma from '@/lib/prisma';
 import { cookies } from "next/headers";
 import { jwtVerify } from "jose";
 import { ClipboardList, Plus } from "lucide-react";
 import Link from "next/link";
 
-const prisma = new PrismaClient();
+
 const JWT_SECRET = new TextEncoder().encode(process.env.JWT_SECRET || 'super-secret-educational-key-2026');
 
 export default async function TareasDocentePage() {
