944467eb2713a25f440659e48bc746b32ced4fe5
lelopezv82-hash <lelopezv82@gmail.com>
2026-05-31 23:03:35 -0500

    deploy: upload get_build_error.js

---
+++ get_build_error.js
@@ -0,0 +1,39 @@
+const https = require('https');
+
+const RENDER_API_KEY = 'rnd_4OnCVuMVqth46XeVlb7kGVW9Aez9';
+const SERVICE_ID = 'srv-d8avvkegvqtc73a4bqa0';
+
+function makeRequest(url) {
+  const urlObj = new URL(url);
+  const options = {
+    hostname: urlObj.hostname,
+    path: urlObj.pathname + urlObj.search,
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': `Bearer ${RENDER_API_KEY}`,
+      'User-Agent': 'node-status-check'
+    }
+  };
+
+  return new Promise((resolve) => {
+    https.get(options, (res) => {
+      let body = '';
+      res.on('data', chunk => body += chunk);
+      res.on('end', () => {
+        try {
+          resolve(JSON.parse(body));
+        } catch (e) {
+          resolve(body);
+        }
+      });
+    });
+  });
+}
+
+async function main() {
+  const deploys = await makeRequest(`https://api.render.com/v1/services/${SERVICE_ID}/deploys?limit=5`);
+  console.log('Deploys:', JSON.stringify(deploys, null, 2));
+}
+
+main();
