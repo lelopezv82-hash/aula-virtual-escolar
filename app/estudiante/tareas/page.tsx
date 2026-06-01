11bab3734fb598622e95b1f0106753cdc300c1b7
lelopezv82-hash <lelopezv82@gmail.com>
2026-05-31 22:23:59 -0500

    deploy: upload app/estudiante/tareas/page.tsx

--- app/estudiante/tareas/page.tsx
+++ app/estudiante/tareas/page.tsx
@@ -24,7 +24,7 @@ export default async function TareasEstudiantePage() {
         where: { studentId }
       }
     },
-    orderBy: { dueDate: "asc" }
+    orderBy: { createdAt: "desc" }
   });
 
   return (
