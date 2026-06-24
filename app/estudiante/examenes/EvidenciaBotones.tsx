e09c5e4d568467ac4f625dde2f7b6828ef365d5b
Estudiante <estudiante@ejemplo.com>
2026-06-24 09:22:52 -0500

    feat: show correct option styling for native exams independently of selection

--- app/estudiante/examenes/EvidenciaBotones.tsx
+++ app/estudiante/examenes/EvidenciaBotones.tsx
@@ -306,7 +306,7 @@ export default function EvidenciaBotones({ exam, submission, isGoogleForm }: Evi
                                   let optBorder = "border-[#dadce0]";
                                   let optText = "text-[#202124]";
 
-                                  if (isSelected && isOptCorrect) {
+                                  if (isOptCorrect) {
                                     optBg = "bg-[#e6f4ea]";
                                     optBorder = "border-[#137333]";
                                     optText = "text-[#137333] font-semibold";
@@ -328,7 +328,7 @@ export default function EvidenciaBotones({ exam, submission, isGoogleForm }: Evi
                                         {isSelected && <div className="h-1.5 w-1.5 rounded-full bg-white" />}
                                       </div>
                                       <span className={`text-[14px] ${optText}`}>{opt.text}</span>
-                                      {isSelected && isOptCorrect && <CheckCircle2 size={16} className="text-[#137333] ml-auto shrink-0" />}
+                                      {isOptCorrect && <CheckCircle2 size={16} className="text-[#137333] ml-auto shrink-0" />}
                                       {isSelected && !isOptCorrect && <X size={16} className="text-[#c5221f] ml-auto shrink-0" />}
                                     </div>
                                   );
