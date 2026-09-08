c7ae5dfa026ac76a4fc8d9d7e0334a9a0b0e3444
Estudiante <estudiante@ejemplo.com>
2026-09-07 21:46:35 -0500

    style: renombrar boton a 'Entregar actividad'

--- app/estudiante/tareas/[id]/page.tsx
+++ app/estudiante/tareas/[id]/page.tsx
@@ -749,7 +749,7 @@ export default function TareaDetallePage({ params }: { params: Promise<{ id: str
                 disabled={loading}
               >
                 {loading ? <Loader2 className="animate-spin" size={18} /> : null}
-                <span>Enviar tarea</span>
+                <span>Entregar actividad</span>
               </button>
             </div>
           </form>
